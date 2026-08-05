2468cefffe5816f0d02318f21ad47c27eaa6ba09
unknown <sunthodeb787@gmail.com>
2026-08-06 02:37:46 +0600

    WEbsite times new eommandsasdxcvcxv

--- index.js
+++ index.js
@@ -551,15 +551,13 @@ async function run() {
               
               body { display: flex; justify-content: center; align-items: flex-start; margin: 0;  font-family: 'ArialEmbedded', sans-serif; -webkit-print-color-adjust: exact; }
 
-              // .page-container { background-color: #fff; padding: 1rem 0rem; width: 760px; position: relative; box-sizing: border-box; margin-bottom: -40px; }
               .page-container { background-color: #fff; padding: 1rem 0rem; width: 760px; position: relative; box-sizing: border-box; margin-bottom: -40px; }
-
               .right-content-wrapper { margin-left: 415px; }
 
               .seal-container { rotate: 1deg; position: absolute; top: 65px; left: 373px; width: 6.5rem; height: auto; z-index: 10; }
               .seal-image { width: 80%; height: auto; border-radius: 9999px; }
               // .certificate-box { border: 1px solid #9ca3af; width: 315px; position: relative; height: 175px; }
-              .certificate-box { border: 1px solid #9ca3af; width: 324px; position: relative; height: 175px; }
+              .certificate-box { border: 1px solid #9ca3af; width: 328px; position: relative; height: 175px; }
 
               .certificate-main { 
                 width: 300px; 
