9e564be0b902f79043506b1d43ed879b34def5f0
unknown <sunthodeb787@gmail.com>
2026-08-06 02:26:36 +0600

    WEbsite times new eommans updae fornt ussejkhjkfgfgasdasdasddsdadasdaxzcxz

--- index.js
+++ index.js
@@ -552,7 +552,7 @@ async function run() {
               body { display: flex; justify-content: center; align-items: flex-start; margin: 0;  font-family: 'ArialEmbedded', sans-serif; -webkit-print-color-adjust: exact; }
 
               // .page-container { background-color: #fff; padding: 1rem 0rem; width: 760px; position: relative; box-sizing: border-box; margin-bottom: -40px; }
-              .page-container { background-color: #fff; padding: 1rem 0rem; width: 780px; position: relative; box-sizing: border-box; margin-bottom: -40px; }
+              .page-container { background-color: #fff; padding: 1rem 0rem; width: 790px; position: relative; box-sizing: border-box; margin-bottom: -40px; }
 
               .right-content-wrapper { margin-left: 415px; }
 
