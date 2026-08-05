4c853ca46dfe17811389c0c3f54de9900c7d1cfb
unknown <sunthodeb787@gmail.com>
2026-08-06 01:59:29 +0600

    WEbsite times new eommans updae fornt ussejkhjk

--- index.js
+++ index.js
@@ -584,7 +584,8 @@ async function run() {
               .header-image-container { position: absolute; top: -0.8rem; left: 48%; transform: translateX(-50%); width: 244px; z-index: 10; }
               .header-image { width: 100%;}
 
-              .data-label-en, .data-value, .data-label-ar { line-height: 1.1; font-size: 10px; }
+              // .data-label-en, .data-value, .data-label-ar { line-height: 1.1; font-size: 10px; }
+              .data-label-en, .data-value, .data-label-ar { line-height: 1.1; font-size: 9px; }
 
               // .data-value { font-family: 'RobotoRegular', serif; white-space: pre-wrap; font-weight: 400; margin-left: -50px; }
               .data-value { font-family: 'Calibribold', serif; white-space: pre-wrap; font-weight: 400; margin-left: -50px; }
@@ -601,7 +602,7 @@ async function run() {
               .footer-line { margin: 0.2rem 0; }
 
               // .footer-mono { font-family: 'TimesEmbedded', serif; letter-spacing: 0.07em; font-size: 0.7rem; font-weight: 600; }
-              .footer-mono { font-family: 'BoldTimesNewRoman', serif; letter-spacing: 0.05em; font-size: 0.7rem; font-weight: 600; }
+              .footer-mono { font-family: 'BoldTimesNewRoman', serif; letter-spacing: 0.04em; font-size: 0.7rem; font-weight: 600; }
               
               .qr-code-image { position: absolute; bottom: 15px; right: 240px; width: 4.5rem; height: 4.5rem; opacity: 0.8; }
               .blockchain-verified { position: absolute; bottom: 40px; left: 20px; }
@@ -611,7 +612,7 @@ async function run() {
               .att { font-family: 'RobotoCondensedMedium', serif; font-weight: bold; }
 
               // .qr{ font-family: 'TimesEmbedded', serif; position: absolute; left: -45px; font-size:0.6rem; }
-              .qr{ font-family: 'BoldTimesNewRoman', serif; position: absolute; left: -45px; font-size:0.7rem; }
+              .qr{ font-family: 'BoldTimesNewRoman', serif; position: absolute; left: -48px; font-size:0.7rem; }
 
               </style>
           </head>
