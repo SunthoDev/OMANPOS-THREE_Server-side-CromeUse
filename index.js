48dcf445dac6c2a932ef1fa269aae67c73076ea3
unknown <sunthodeb787@gmail.com>
2026-08-11 22:28:29 +0600

    Update font gfdgfd

--- index.js
+++ index.js
@@ -669,7 +669,7 @@ async function run() {
               .att { font-family: 'RobotoCondensedMedium', serif; font-weight: bold; }
 
               // .qr{ font-family: 'TimesEmbedded', serif; position: absolute; left: -45px; font-size:0.6rem; }
-              .qr{ font-family: 'BoldTimesNewRoman', serif; position: absolute; left: -64px; font-size:0.8rem; }
+              .qr{ font-family: 'BoldTimesNewRoman', serif; position: absolute; left: -68px; font-size:0.8rem; }
 
               </style>
           </head>
