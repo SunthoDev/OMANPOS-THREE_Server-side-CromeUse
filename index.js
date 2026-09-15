05ece3502cdbe4ec808adc3a4402966aa268d388
unknown <sunthodeb787@gmail.com>
2026-09-16 05:40:20 +0600

    Update website font to use (calibri)....cxcxc

--- index.js
+++ index.js
@@ -628,7 +628,7 @@ async function run() {
 
               // .data-value { font-family: 'Calibribold', serif; white-space: pre-wrap; font-weight: 400; margin-left: -45px; }
               .data-value { 
-              font-family: 'Calibri', 'Calibribold', Roboto, sans-serif; 
+              font-family: Calibri, Roboto, sans-serif; 
               white-space: pre-wrap; 
               font-weight: 400; 
               margin-left: -45px; }
@@ -636,7 +636,7 @@ async function run() {
               // .data-label-en { font-family: 'Calibribold', serif; font-weight: 400; }
               .data-label-en { 
               // font-family: Calibri, Roboto, sans-serif; 
-              font-family: 'Calibri', 'Calibribold', Roboto, sans-serif; 
+              font-family: Calibri, Roboto, sans-serif; 
               font-weight: 400; }
 
 
