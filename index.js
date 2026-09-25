f901d5911ad6f5f11a7208312fd388447fcdd926
unknown <sunthodeb787@gmail.com>
2026-09-26 00:16:28 +0600

    igbjknjbkgnbmg

--- index.js
+++ index.js
@@ -628,7 +628,7 @@ async function run() {
               // .data-label-en, .data-value, .data-label-ar { line-height: 1.2; font-size: 10px; }
               .data-label-en, .data-value, .data-label-ar { line-height: 1.2; font-size: 9.4px; }
 
-              .data-value { font-family: 'Calibribold', serif; white-space: pre-wrap; font-weight: 400; margin-left: -45px; }
+              .data-value { font-family: 'Calibribold', serif; white-space: pre-wrap; font-weight: 400; margin-left: -41px; }
 
               .data-label-en { font-family: 'Calibribold', serif; font-weight: 400; }
 
