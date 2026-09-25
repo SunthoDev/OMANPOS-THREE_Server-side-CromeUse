fdd6d8c3d9ab72104d5838af05d8aab71405bb21
unknown <sunthodeb787@gmail.com>
2026-09-25 23:56:18 +0600

    ghbhbgvcv

--- index.js
+++ index.js
@@ -621,7 +621,7 @@ async function run() {
               }
 
               // .header-image-container { position: absolute; top: -0.8rem; left: 48%; transform: translateX(-50%); width: 244px; z-index: 10; }
-              .header-image-container { position: absolute; top: -0.8rem; left: 48%; transform: translateX(-50%); width: 250px; z-index: 10; }
+              .header-image-container { position: absolute; top: -0.8rem; left: 48%; transform: translateX(-50%); width: 254px; z-index: 10; }
 
               .header-image { width: 100%;}
 
